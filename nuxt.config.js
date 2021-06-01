6ca26304c12fbfdd025ff8f0a9ffb4afaa0c5280
William Chong <me@williamchong.cloud>
2021-06-01 16:10:44 +0800

    💬 Update site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Lunie 3',
+    title: 'LikeCoin - Lunie 3',
     htmlAttrs: {
       lang: 'en',
     },
